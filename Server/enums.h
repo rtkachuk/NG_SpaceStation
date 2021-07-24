2da25319dbf9c4bde495a2d645fa07ee13401b83
Roman Tkachuk <tkachukroman64@gmail.com>
2021-07-24 17:09:17 +0300

    Implemented operator < for position struct

--- Server/enums.h
+++ Server/enums.h
@@ -1,10 +1,15 @@
 #ifndef ENUMS_H
 #define ENUMS_H
 
+#include <tuple>
+
 struct position
 {
     int x;
     int y;
+	bool operator<( const position& other) const {
+			 return std::tie(x,y) < std::tie(other.x,other.y);
+		}
 };
 
 enum playerMovements {
